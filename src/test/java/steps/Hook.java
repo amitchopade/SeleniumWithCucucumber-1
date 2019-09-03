259aa59c937c62190c7b8d4ba361fadfdab4a614
AmitAmit <ameetchopade@gmail.com>
2019-09-03 16:48:35 +0800

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -28,7 +28,7 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
 
-       System.setProperty("webdriver.firefox.marionette", "/bin/firefox");
+       System.setProperty("webdriver.firefox.marionette", "/root/selenium/geckodriver");
         base.Driver = new FirefoxDriver();
 
 
